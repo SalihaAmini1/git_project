5015e641a58b89c6998432d33f46270f4525ab80
SalihaAmini1 <Saliha.amini123@gmail.com>
2023-08-18 16:10:37 -0400

    changes from new branch

---
+++ src/git_project/Amini.java
@@ -0,0 +1,10 @@
+package git_project;
+
+public class Amini {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("new changes in branch Amini");
+	}
+
+}
